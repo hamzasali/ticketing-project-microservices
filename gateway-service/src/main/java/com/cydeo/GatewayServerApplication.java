703c1c201f244c1e72712288c2ce38a8158ba5ce
Hamza Ali <hamzamughal2145@gmail.com>
2023-01-24 19:15:48 -0500

    Gateway

--- gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
@@ -3,6 +3,9 @@ package com.cydeo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,7 +16,7 @@ public class GatewayServerApplication {
         SpringApplication.run(GatewayServerApplication.class,args);
     }
 
-    //    @Bean
+//        @Bean
 //    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 //        return builder.routes()
 //                .route(p -> p
