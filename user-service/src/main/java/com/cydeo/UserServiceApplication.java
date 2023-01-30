578133b7ec8795194bb8348663c76e23f4f59a59
Hamza Ali <hamzamughal2145@gmail.com>
2023-01-30 11:29:09 -0500

    Swagger

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -5,19 +5,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(servers = {
+        @Server(url = "/user-service")
+}, info =
+@Info(title = "Ticketing App", version = "1.0", description = "User Service API"))
 public class UserServiceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(UserServiceApplication.class,args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
