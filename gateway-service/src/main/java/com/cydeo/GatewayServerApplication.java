fef634094811e7af9e102a9225b585422c38c86b
Hamza Ali <hamzamughal2145@gmail.com>
2023-01-30 12:08:00 -0500

    Revert "Swagger"
    This reverts commit bc23fa28a1c22522bdef269536fdd502e9770166.

--- gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServerApplication.java
@@ -1,16 +1,11 @@
 package com.cydeo;
 
-import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -18,10 +13,10 @@ public class GatewayServerApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(GatewayServerApplication.class, args);
+        SpringApplication.run(GatewayServerApplication.class,args);
     }
 
-    //        @Bean
+//        @Bean
 //    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 //        return builder.routes()
 //                .route(p -> p
@@ -41,19 +36,4 @@ public class GatewayServerApplication {
 //                        .uri("lb://task-service"))
 //                .build();
 //    }
-    @Bean
-    public List<GroupedOpenApi> apiList(SwaggerUiConfigParameters swaggerUiConfigParameters,
-                                        RouteDefinitionLocator routeDefinitionLocator) {
-        List<GroupedOpenApi> groupedOpenApis = new ArrayList<>();
-        List<RouteDefinition> definitions = routeDefinitionLocator
-                .getRouteDefinitions().collectList().block();
-
-        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")
-        ).forEach(routeDefinition -> {
-            String name = routeDefinition.getId().replaceAll("-service", "");
-            swaggerUiConfigParameters.addGroup(name);
-            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
-        });
-        return groupedOpenApis;
-    }
 }
